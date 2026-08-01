1632c0af4530d6aa24d427fa23d114c18ded8852
blade134 <blade134@qq.com>
2026-08-01 11:30:28 +0800

    Add files via upload

---
+++ inference_gpu.py
@@ -0,0 +1,30 @@
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_path = './LLM/out-12block-tomatoes-2'
+tokenizer = GPT2Tokenizer.from_pretrained(model_path)
+model = GPT2LMHeadModel.from_pretrained(model_path).to(device)
+
+input_text = "Unite States has 51 states,"
+
+inputs = tokenizer(input_text, return_tensors='pt').to(device)
+print(inputs)
+
+outputs = model.generate(
+    inputs.input_ids,
+    attention_mask=inputs.attention_mask,
+    max_new_tokens=200,
+    num_return_sequences=2,
+    pad_token_id=tokenizer.eos_token_id,
+    do_sample=True,
+    top_k=50,
+    top_p=0.95,
+    temperature=1.0,
+    no_repeat_ngram_size=2,
+)
+
+for i, output in enumerate(outputs):
+    generated_text = tokenizer.decode(output, skip_special_tokens=True)
+    print(f"Sequence {i+1}: {generated_text}\n")
